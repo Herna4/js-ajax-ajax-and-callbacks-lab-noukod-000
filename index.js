db2ea98a5d0c8034c9ba60212a768ffd243b67b1
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-23 02:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function (){
 function searchRepositories(){
   const searchTerms = document.getElementById('searchTerms').value;
   $.get('https://api.github.com/search/repositories?q' + searchTerms, (response) => {
-    
+     
   });
   
 }
